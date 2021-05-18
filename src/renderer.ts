aa2c04c3480e3038ae9534ab694e3f213ddc0165
Brenek Harrison <brenekharrison@gmail.com>
2021-05-18 15:14:33 -0600

    Remove unused code

--- src/renderer.ts
+++ src/renderer.ts
@@ -2,15 +2,3 @@ function getBlobURL(code, type) {
 	const blob = new Blob([code], { type })
 	return URL.createObjectURL(blob)
 }
-
-// let num = 0;
-// let blob = getBlobURL(`<html><body>${num}</body></html>`, "text/html");
-// console.log(blob);
-// document.getElementById("intFrame").src = blob;
-
-// setInterval(() => {
-// 	let blob = getBlobURL(`<html><head></head><body>${num}</body></html>`, "text/html");
-// 	console.log(blob);
-// 	document.getElementById("intFrame").src = blob;
-// 	num++;
-// }, 1000);
