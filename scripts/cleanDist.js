aa94416c10a9f04ece2dbde962640da4b4781aff
Brenek Harrison <brenekharrison@gmail.com>
2021-05-18 19:19:05 -0600

    Clean dist before running when using npm run start

---
+++ scripts/cleanDist.js
@@ -0,0 +1,26 @@
+// Yoinked from https://stackoverflow.com/a/52526549/13298346
+
+var fs = require('fs');
+
+function deleteFolderRecursive(path) {
+  if (fs.existsSync(path) && fs.lstatSync(path).isDirectory()) {
+    fs.readdirSync(path).forEach(function(file, index){
+      var curPath = path + "/" + file;
+
+      if (fs.lstatSync(curPath).isDirectory()) { // recurse
+        deleteFolderRecursive(curPath);
+      } else { // delete file
+        fs.unlinkSync(curPath);
+      }
+    });
+
+    console.log(`Deleting directory "${path}"...`);
+    fs.rmdirSync(path);
+  }
+};
+
+console.log("Cleaning working tree...");
+
+deleteFolderRecursive("./dist");
+
+console.log("Successfully cleaned working tree!");
