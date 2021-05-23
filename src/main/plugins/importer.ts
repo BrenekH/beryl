448792e3dd3853943580dd6e6ec9a6d68690c6df
Brenek Harrison <brenekharrison@gmail.com>
2021-05-23 15:22:15 -0600

    Extract zip to temporary directory

--- src/main/plugins/importer.ts
+++ src/main/plugins/importer.ts
@@ -1,3 +1,32 @@
+import * as path from "path"
+import extract = require("extract-zip")
+import { mkdirSync } from "fs"
+import { tmpdir } from "os"
+import { platform, env } from "process"
+
+const tempDir = tmpdir()
+const pluginStorageDir = getOSPluginStorageDir()
+
 export function ImportBIPA(fileLocation: string) {
-	console.log(fileLocation)
+	extract(fileLocation, { dir: path.resolve(tempDir, path.basename(fileLocation)) }).then(() => {
+		console.log("Extract complete")
+		// TODO: Move extracted files to pluginStorageDir
+	}).catch((err: any) => {
+		console.error(err)
+	})
+}
+
+function getOSPluginStorageDir(): string {
+	let pluginStorageDir
+	if (platform === "win32") {
+		pluginStorageDir = `${env.LOCALAPPDATA}/Beryl/installedPlugins`
+	} else {
+		pluginStorageDir = `${env.HOME}/.beryl/installedPlugins`
+	}
+
+	try {
+		mkdirSync(pluginStorageDir)
+	} catch (e: any) {}
+
+	return pluginStorageDir
 }
