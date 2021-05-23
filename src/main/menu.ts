0e3380ba0a9fe579b78cc4c22ea7ad9825cdc9d7
Brenek Harrison <brenekharrison@gmail.com>
2021-05-23 14:29:25 -0600

    Menu item for import plugins

--- src/main/menu.ts
+++ src/main/menu.ts
@@ -1,5 +1,6 @@
 import { BrowserWindow, dialog, Menu, MenuItem } from "electron"
 import { OpenDialogReturnValue } from "electron/main"
+import { ImportBIPA } from "./plugins/importer"
 import Main from "./main"
 
 export function createMenu(browserWindow: BrowserWindow) : Menu {
@@ -7,42 +8,69 @@ export function createMenu(browserWindow: BrowserWindow) : Menu {
 
 	menu.append(new MenuItem({
 		label: "File",
-		submenu: [{
-			label: "Open Profile",
-			accelerator: "CommandOrControl+O",
-			click: () => {
-				dialog.showOpenDialog(browserWindow, {
-					title: "Open Profile",
-					buttonLabel: "Open",
-					filters :[
-						{name: "JSON", extensions: ["json"]},
-						{name: "All Files", extensions: ["*"]}
-					   ],
-					properties: ["openFile"]
-				}).then((dialogReturn: OpenDialogReturnValue) => {
-					if (!dialogReturn.canceled) {
-						Main.loadProfile(dialogReturn.filePaths[0])
-					}
-				})
+		submenu: [
+			{
+				label: "Open Profile",
+				accelerator: "CommandOrControl+O",
+				click: () => {
+					dialog.showOpenDialog(browserWindow, {
+						title: "Open Profile",
+						buttonLabel: "Open",
+						filters: [
+							{name: "JSON", extensions: ["json"]},
+							{name: "All Files", extensions: ["*"]}
+						],
+						properties: ["openFile"]
+					}).then((dialogReturn: OpenDialogReturnValue) => {
+						if (!dialogReturn.canceled) {
+							Main.loadProfile(dialogReturn.filePaths[0])
+						}
+					})
+				},
 			},
-		}]
+			{
+				label: "Import .bipa",
+				accelerator: "CommandOrControl+I",
+				click: () => {
+					dialog.showOpenDialog(browserWindow, {
+						title: "Import .bipa",
+						buttonLabel: "Open",
+						filters: [
+							{name: "BIPA", extensions: ["bipa"]},
+							{name: "All Files", extensions: ["*"]}
+						],
+						properties: ["openFile", "multiSelections"]
+					}).then((dialogReturn: OpenDialogReturnValue) => {
+						if (!dialogReturn.canceled) {
+							dialogReturn.filePaths.forEach((path: string) => {
+								ImportBIPA(path)
+							})
+						}
+					})
+				},
+			},
+		]
 	}))
 
 	menu.append(new MenuItem({
 		label: "View",
-		submenu: [{
-			accelerator: "F11",
-			role: "togglefullscreen"
-		}]
+		submenu: [
+			{
+				accelerator: "F11",
+				role: "togglefullscreen"
+			},
+		]
 	}))
 
 	menu.append(new MenuItem({
 		label: "About",
-		submenu: [{
-			label: "Show DevTools",
-			accelerator: 'CommandOrControl+Shift+I',
-			click: () => { browserWindow.webContents.openDevTools() }
-		}]
+		submenu: [
+			{
+				label: "Show DevTools",
+				accelerator: 'CommandOrControl+Shift+I',
+				click: () => { browserWindow.webContents.openDevTools() }
+			}
+		]
 	}))
 
 	return menu
